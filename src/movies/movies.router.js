de7890c1841d5e03dbfe04d2efe9b9ab5b0128af
Ryan Prosser <ryanpross77@gmail.com>
2023-09-28 12:33:44 -0400

    methodNotAllowed and fixed migration

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -7,8 +7,8 @@ const methodNotAllowed = require("../errors/methodNotAllowed")
 
 router.route("/").get(controller.list).all(methodNotAllowed)
 router.route("/:movieId").get(controller.read).all(methodNotAllowed)
-router.route("/:movieId/theaters").get(controller.moviesInTheaters)
-router.route("/:movieId/reviews").get(controller.reviewsWithCritic)
+router.route("/:movieId/theaters").get(controller.moviesInTheaters).all(methodNotAllowed)
+router.route("/:movieId/reviews").get(controller.reviewsWithCritic).all(methodNotAllowed)
 
 
 module.exports = router
